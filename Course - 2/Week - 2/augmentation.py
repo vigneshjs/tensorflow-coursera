67f18a159bccc26398a3d9ee382644e76a3fe056
vigneshj <vignesh@ocularred.com>
2019-12-26 17:32:45 +0530

    Implementing Augmentation

---
+++ Course - 2/Week - 2/augmentation.py
@@ -0,0 +1,105 @@
+import matplotlib.pyplot as plt
+from tensorflow_core.python.keras.api import keras
+import os
+
+base_dir = '/home/user/learning/tensorflow-coursera/Datasets'
+training_dir = os.path.join(base_dir, 'horse-or-human')
+validation_dir = os.path.join(base_dir, 'validation-horse-or-human')
+
+training_datagen = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1/255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+training_generator = training_datagen.flow_from_directory(
+    training_dir,
+    batch_size=128,
+    target_size=(300, 300),
+    class_mode='binary'
+)
+
+validation_datagen = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1/255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(300, 300),
+    batch_size=32,
+    class_mode='binary'
+)
+
+model = keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    keras.layers.Conv2D(16, (3, 3), activation='relu',
+                        input_shape=(300, 300, 3)),
+    keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    # The third convolution
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    # The fourth convolution
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    # The fifth convolution
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(2, 2),
+    # Flatten the results to feed into a DNN
+    keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer=keras.optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+history = model.fit_generator(
+    training_generator,
+    steps_per_epoch=8,
+    epochs=1,
+    validation_data=validation_generator,
+    validation_steps=8,
+    verbose=1
+)
+
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
